e9c2a5b2e57bd17092b01e85e2581889247d2ee8
stefanieyuxi <chenxicheer@gmail.com>
2016-06-21 01:46:04 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
-require "active_support"
-require 'active_support/core_ext/object/blank'
 require 'rspec/rails'
 require 'rspec/autorun'
 
